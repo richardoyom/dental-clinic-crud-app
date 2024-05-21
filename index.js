65107a9606216c210c9395ff01be98acf016e44b
richard oyom <richard.oyom@yahoo.co.uk>
2024-05-21 14:25:13 +0100

    second commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.listen(PORT, () => {
 });  
 
 
-mongoose.connect("mongodb+srv://richardoyom:e6dZFEqXXb2GX2D8@cluster0.4x10vkh.mongodb.net/dental-clinic?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://richardoyom:<PASSWORD>@cluster0.4x10vkh.mongodb.net/dental-clinic?retryWrites=true&w=majority&appName=Cluster0")
 .then(() =>{ 
     console.log("connected to database");
 })
